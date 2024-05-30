f51045a42360a059d1c3476a2b7461b0ec55f867
Sushil Jhawar <sujhawar@cisco.com>
2024-05-30 10:50:16 -0700

    Updated file info

--- inc_cisco/sai_cisco_custom_attr.h
+++ inc_cisco/sai_cisco_custom_attr.h
@@ -1,7 +1,21 @@
 /* ----------------------------------------------------------------
- * Copyright (c) 2024 by Cisco Systems, Inc.
+ * Copyright (c) 2024 by Cisco Systems, Inc.  and its affiliates
  * All rights reserved.
- * ----------------------------------------------------------------- */
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *    not use this file except in compliance with the License. You may obtain
+ *    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+ *    CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ *    LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+ *    FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ *    See the Apache Version 2.0 License for specific language governing
+ *    permissions and limitations under the License.
+ *
+ * ---------------------------------------------------------------- */
+
 
 #ifndef __SAI_CISCO_CUSTOM_ATTR_H__
 #define __SAI_CISCO_CUSTOM_ATTR_H__
